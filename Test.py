f9e76a139afad6c49d0c9d42221a9a317f13d6ec
maheedhar1208 <88898828+maheedhar1208@users.noreply.github.com>
2021-11-23 11:42:27 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ class Testfib(unittest.TestCase):
     def test_list_int(self):
         data=9
         result=Fibonacci(data)
-        self.assertEqual(result,34)
+        self.assertEqual(result,35)
 
 if __name__ == '__main__':
     unittest.main()
